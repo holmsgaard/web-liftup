4a6834ea2327ce03269ff9deb828bc7da03e5607
holmsgaard <holmsgaard.peter@gmail.com>
2014-01-23 12:47:11 +0100

    Updated fonts etc.

--- js/core.js
+++ js/core.js
@@ -11,12 +11,51 @@ var jcd = {
     }
 }
 
+
+
+
+
+
 $(window).on('resize', function(e) {
     jcd.layout.resizeContainer();
 });
 
+
 $().ready(function() {
     jcd.layout.resizeContainer();
+    
+    // Enably stick menu
+    (function() {
+        var position = document.body.scrollTop,
+            $nav = $('#top-wrapper').clone(),
+            $body = $('body'),
+            navExists = false,
+            triggerPos = 300;
+        
+        $nav.attr('id', 'top-wrapper-sticky').find('.toplogo').find('img').attr('src', 'img/logo_small.png');
+        
+        $(document).on('scroll', function(e) {
+            position = document.body.scrollTop;
+            if (position > triggerPos && !navExists) {
+                navExists = true;
+                $body.prepend($nav);
+                setTimeout(function() {
+                    $nav.addClass('top-wrapper-sticky-active');
+                }, 100);
+            }
+            else if (position > triggerPos && navExists) {
+                if (!$nav.hasClass('top-wrapper-sticky-active')) {
+                    setTimeout(function() {
+                        $nav.addClass('top-wrapper-sticky-active');
+                    }, 100);
+                }
+            }
+            else {
+                $nav.removeClass('top-wrapper-sticky-active');
+            }
+        });
+    })();
+
 });
 
 $(document).foundation();
